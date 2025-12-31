3d5c6ce0f827d18db07d335246dacee790bfe7f1
shalomarputhasingh <shalomdevofficial@gmail.com>
2025-12-31 23:10:13 +0530

    fix: made image upload as mandatory

--- citizen/forms.py
+++ citizen/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from core.models import Issue
 
 class IssueForm(forms.ModelForm):
-    image = forms.ImageField(required=False, help_text="Upload an image of the issue")
+    image = forms.ImageField(required=True, help_text="Upload an image of the issue")
 
     class Meta:
         model = Issue
